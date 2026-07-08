c3daf618c8c8f027edba4531c610cde6f136f815
3 <113243262+lancet-scripts@users.noreply.github.com>
2026-07-08 03:30:54 +0300

    Create LuckySettings.lua

---
+++ LuckySettings.lua
@@ -0,0 +1,29 @@
+local Config = {
+    TPStepDistance = 30,
+    TPDelay = 0,
+    GrabRadius = 30,
+    Offset = CFrame.new(0, -1.5, 3),
+    BlacklistTime = 12,
+    PostGrabWait = 0.4,
+    ChunkLoadWait = 0.65,
+    SpiralSpacing = 150,
+    MaxSpiralRadius = 3000,
+    YHeight = 25,
+    EnableFarming = false,
+    EnableSelling = false,
+    EnableAutoBuy = false,
+    EnableStreamingBypass = true,
+    EnableNoclip = true,
+    EnableESP = true,
+    EnableAutoHop = false,
+    HopIfNoItemsFor = 15,
+    WebhookURL = "",
+    ItemsCollected = 0,
+    BoughtLuckyArrows = 0,
+    TimeFarmed = 0,
+    WebhookLogInterval = 50,
+    TotalItemsSession = 0,
+    StartTime = tick()
+}
+
+return Config
