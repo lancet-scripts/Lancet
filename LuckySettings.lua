e4dbc377d5cc475e3bba23b5065bc147f06067a6
3 <113243262+lancet-scripts@users.noreply.github.com>
2026-07-08 07:18:59 +0300

    dwdwdwdwdwdwdw
    dwdwdwdwdwdw

--- LuckySettings.lua
+++ LuckySettings.lua
@@ -1,8 +1,8 @@
 local Config = {
     TPStepDistance = 45,
     TPDelay = 0,
-    GrabRadius = 50,
-    Offset = CFrame.new(0, -2, 0),
+    GrabRadius = 45,
+    Offset = CFrame.new(0, -3, 0),
     BlacklistTime = 12,
     PostGrabWait = 0.05,
     ChunkLoadWait = 0.4,
